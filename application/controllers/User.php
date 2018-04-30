64d82c05d9e0304377dfdf591be5e14c0609bd20
syafiaa01 <fiiaabida1103@gmail.com>
2018-04-30 09:05:15 +0700

    third commit

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -1,12 +1,7 @@
 <?php
 
 class User extends CI_Controller {
-	//function auto run
-	function __construct(){
-		parent::__construct();
-		//load model
-		$this->load->model("user_m");
-    }
+
 
 	function index(){
 		$this->load->view("form_user_v");
@@ -14,16 +9,9 @@ class User extends CI_Controller {
 
 	function add() {
 		//create data array
-		$data = array(
-			"username" => $this->input->post("username"),
-			"password" => $this->input->post("password"),
-			"fullname" => $this->input->post("fullname"),
-			"level" => $this->input->post("level")
-			);
-
-		//check data before save
-		var_dump($data);
-		//save data to database
-		$this->user_m->add($data);
+	echo $this->input->post("username") . "<br/>";
+	echo $this->input->post("password") . "<br/>";
+	echo $this->input->post("fullname") . "<br/>";
+	echo $this->input->post("level") . "<br/>";
 	}
 }
