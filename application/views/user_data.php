09ba002c2929fb46b32d71315c2f72fc06267dc3
syafiaa01 <fiiaabida1103@gmail.com>
2018-05-06 16:51:05 +0700

    fifth commit

---
+++ application/views/user_data.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<table>
+		<tr>
+			<th>No</th>
+			<th>Username</th>
+			<th>Password</th>
+			<th>Level</th>
+			<th>Aksi</th>
+		</tr>
+	</table>
+</body>
+</html>
